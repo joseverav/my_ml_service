4279f2f761c28322c4c5b3d7464e143268958d90
joseverav <joseverav30@gmail.com>
2023-02-05 18:38:50 -0700

    added superuser and registered the Endpoint model

--- backend/server/apps/endpoints/admin.py
+++ backend/server/apps/endpoints/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+
+# Import models 
+from apps.endpoints.models import Endpoint #, MLAlgorithm, MLRequest
+
+# Create a class for the admin-model integration
+class MLAdmin(admin.ModelAdmin):
+
+    # add the fields of the model here
+    list_display = ("name", "owner" , "created_at")
+
+# Register model class and admin model class
+admin.site.register(Endpoint, MLAdmin)
