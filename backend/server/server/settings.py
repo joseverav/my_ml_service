db4c6f96a461ab492f9d39c7055302084481accf
Jose Vera <jvera@reaveley.com>
2023-02-12 14:42:56 -0700

    add localhost to allowed and change app.js to class based

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^lw0)9982b8nt^^-t34967jnl(n7iojl!q+!5xojw9mr6u0h9f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
 
 # CORS_ORIGIN_ALLOW_ALL: If True, all origins will be accepted 
 # (not use the whitelist below). Defaults to False.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-const appTitle = 'MLRepo';
+const appTitle = "MLRepo";
 
-function LandingPage() {
-  return (
-    <div>
-      <header>
-        <h1>Welcome to {appTitle}</h1>
-        <nav>
-          <a href="#about">Algorithms</a>
-          <a href="#products">Products</a>
-          <a href="#contact">Contact</a>
-        </nav>
-      </header>
-      <main>
-        <h2>Leading the way in AI accessibility</h2>
-        <p>{appTitle} is dedicated to advancing human capability by leveraging digital intelligence.</p>
-      </main>
-      <footer>
-        <p style={{float: 'left'}}>{appTitle} &copy; {new Date().getFullYear()}</p>
-      </footer>
-    </div>
-  );
+class LandingPage extends Component {
+  state = {
+    requests: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:8000/api/v1/mlrequests/")
+      .then(res => {
+        this.setState({ requests: res.data });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to {appTitle}</h1>
+          <nav>
+            <a href="#about">Algorithms</a>
+            <a href="#products">Products</a>
+            <a href="#contact">Contact</a>
+          </nav>
+        </header>
+        <main>
+          <h2>Leading the way in AI accessibility</h2>
+          <p>
+            {appTitle} is dedicated to advancing human capability by leveraging
+            digital intelligence.
+          </p>
+        </main>
+        <footer>
+          <p style={{ float: "left" }}>
+            {appTitle} &copy; {new Date().getFullYear()}
+          </p>
+        </footer>
+        <div>
+          {this.state.requests.map(requests => (
+            <p key={requests.id}>{requests.name}</p>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
