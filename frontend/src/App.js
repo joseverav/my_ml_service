9cfdad65d6b542c5ff9e96ec96a6b5f51a57cf7c
Jose Vera <jvera@reaveley.com>
2023-02-15 21:25:37 -0700

    dark mode implementation for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,53 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./index.css";
 
 const appTitle = "MLRepo";
 
 class LandingPage extends Component {
   state = {
-    requests: []
+    requests: [],
+    isDarkMode: false,
   };
 
   componentDidMount() {
     axios
       .get("http://localhost:8000/api/v1/mlrequests")
-      .then(res => {
+      .then((res) => {
         this.setState({ requests: res.data });
-        console.log(res.data)
+        console.log(res.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   }
 
+  handleDarkModeToggle = () => {
+    this.setState((prevState) => ({
+      isDarkMode: !prevState.isDarkMode,
+    }));
+  };
+
   render() {
     const lastRequest = this.state.requests[this.state.requests.length - 1];
+    const { isDarkMode } = this.state;
 
     return (
-      <div>
+      <div className={`app ${isDarkMode ? "dark-mode" : ""}`}>
         <header>
           <h1>Welcome to {appTitle}</h1>
-          <nav>
-            <a href="#incomeclassifier">Income Classifier</a>
-            <a href="#contact">Contact</a>
-          </nav>
+          <div className="nav-container">
+            <nav>
+              <a href="#incomeclassifier">Income Classifier</a>
+              <a href="#contact">Contact</a>
+              <img
+                src="/dark-mode-icon.png"
+                alt="Toggle Dark Mode"
+                className="dark-mode-toggle"
+                onClick={this.handleDarkModeToggle}
+              />
+            </nav>
+          </div>
         </header>
         <main>
           <h2>Leading the way in AI accessibility</h2>
@@ -38,7 +55,12 @@ class LandingPage extends Component {
             {appTitle} is dedicated to advancing human capability by leveraging
             digital intelligence.
           </p>
-          {lastRequest && <p> {lastRequest.id} : {lastRequest.response}</p>}
+          {lastRequest && (
+            <p>
+              {" "}
+              {lastRequest.id} : {lastRequest.response}
+            </p>
+          )}
         </main>
         <footer>
           <p>
@@ -51,6 +73,7 @@ class LandingPage extends Component {
 }
 
 export default LandingPage;
+
 
 /* Use to render all objects in get request:
 {this.state.requests.map((request) => (
